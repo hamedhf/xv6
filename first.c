220b06d1c23be3ebb1270e8d1a1c26c78ee55872
Hamed Feizabadi <hamedfz19@gmail.com>
2022-06-30 13:37:19 +0430

    first xv6 user program

---
+++ first.c
@@ -0,0 +1,12 @@
+//A Simple C program
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+//passing command line arguments
+
+int main(int argc, char *argv[])
+{
+  printf(1, "My first xv6 program\n");
+  exit();
+}
